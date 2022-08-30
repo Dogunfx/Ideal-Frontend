a0ca181f2a95153ef70e3c70ccdfc04b10754bcf
Dogun Oghenekobiruo <dogunfx@gmail.com>
2022-08-30 13:58:37 -0700

    hardcoded database entry in the todo app

--- todo app/day two/js/db.js
+++ todo app/day two/js/db.js
@@ -1,13 +1,14 @@
-users = [
+let users = [
   {
     user_name: "blessing",
     user_id: 1,
     user_password: "65413",
+    email: "blessing@gmail.com",
   },
   {
     user_name: "emma",
     user_id: 2,
-    user_password: "09865",
+    user_password: "09865@",
   },
   {
     user_password: "12345",
@@ -15,3 +16,29 @@ users = [
     user_name: "dogunfx",
   },
 ];
+
+function checkAuth(user_name, user_password) {
+  let hasFound = false;
+
+  users.forEach((user) => {
+    if (user.user_name == user_name && user.user_password == user_password) {
+      hasFound = true;
+      return;
+    }
+  });
+  return hasFound;
+}
+
+function addNewUser(user) {
+  users.push(user);
+}
+
+function getNumberOfUser() {
+  return users.length;
+}
+
+function showUsers() {
+  users.forEach((user) => {
+    alert(user.user_name);
+  });
+}
--- todo app/day two/js/script.js
+++ todo app/day two/js/script.js
@@ -3,17 +3,43 @@ function toggleDialog() {
   dialog_bg.classList.toggle("show");
 }
 
-document.getElementById("login_form").onsubmit = checkdetails;
+document.getElementById("login_form").onsubmit = checkDetails;
 
-function checkdetails(evt) {
+// creating a button element to add a user
+var create_btn = document.createElement("button");
+create_btn.innerHTML = "Yes";
+// create_btn.type = "submit";
+create_btn.classList.add("primary-button");
+// button to add a new user code ends here
+
+function checkDetails(evt) {
+  let msg = document.getElementById("msg");
   let username = document.getElementById("username").value;
   let userpassword = document.getElementById("userpassword").value;
-
-  if (username == db_user && userpassword == db_password) {
-    alert("correct");
-  } else {
-    document.getElementById("msg").innerHTML =
-      "Credentials Not correct, would you like to create a new profile  <button class='primary-button'> Yes </button>";
+  var isValid = checkAuth(username, userpassword);
+  if (!isValid) {
+    msg.innerHTML = `Credentials Not correct, would you like to create a new profile`;
+    msg.appendChild(create_btn);
     evt.preventDefault();
   }
 }
+
+create_btn.onclick = function () {
+  let username = document.getElementById("username").value;
+  let userpassword = document.getElementById("userpassword").value;
+
+  var newUser = {
+    user_name: username,
+    user_password: userpassword,
+    user_id: getNumberOfUser(),
+  };
+
+  addNewUser(newUser);
+  // document.getElementById("login_form").dispatchEvent(new Event("onSubmit"));
+
+  // document.getElementById("login_form;
+
+  msg.innerHTML = "Account Created, Click continue to enter start";
+
+  msg.className = "success";
+};
